6e33c40bddf334b38b8cf79d0ad856133125535c
Srsanchxz <79011986+Srsanchxz@users.noreply.github.com>
2023-03-21 14:36:29 -0600

    agregando menu mobile y agregando media queryes

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-
+const btnMenuMobile = document.querySelector(".btn-menu-mobile");
+const mobileMenu = document.querySelector(".mobile-menu");
 menuEmail.addEventListener("click", toggleDesktopMenu); //le dedimos que queremos que ejecute la clase 'toggleDektopMenu' cuando le de un click
+btnMenuMobile.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
