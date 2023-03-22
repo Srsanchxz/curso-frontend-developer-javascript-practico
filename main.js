b66f4bb19bf30a1ccab69b7d98e019f6212d4ef8
Srsanchxz <79011986+Srsanchxz@users.noreply.github.com>
2023-03-21 21:35:44 -0600

    creando componente para cards

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+/*NAVEGACION ------------------------------------------------------------*/
 const menuEmail = document.querySelector(".navbar-email"); /*el boton*/
-const desktopMenu = document.querySelector(".desktop-menu");
+const desktopMenu =
+  document.querySelector(".desktop-menu"); /*lo que desplegara*/
 const btnMenuMobile = document.querySelector(".btn-menu-mobile");
 const mobileMenu = document.querySelector(".mobile-menu");
 const btnCarrito = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu); //le dedimos que queremos que ejecute la clase 'toggleDektopMenu' cuando le de un click
 btnMenuMobile.addEventListener("click", toggleMobileMenu);
@@ -41,4 +44,83 @@ function toggleBtnCarrito() {
     desktopMenu.classList.add("inactive");
   }
   aside.classList.toggle("inactive");
+}
+
+/*CREANDO COMPONENTES DE PRODUCTOS---------------------------------------*/
+
+const productList = [];
+productList.push({
+  name: "bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "shirt",
+  price: 80,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Snapback",
+  price: 55,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+/* <section class="main-container">
+        <div class="cards-container">
+    
+          <div class="product-card">
+            <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+            <div class="product-info">
+              <div>
+                <p>$120,00</p>
+                <p>Bike</p>
+              </div>
+              <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="">
+              </figure>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    */
+for (product of productList) {
+  const productCard = document.createElement("div");
+  productCard.classList.add("product-card");
+
+  // product = {name, price, image} -> product.image
+  const productImg = document.createElement("img");
+  productImg.setAttribute("src", product.image);
+
+  const productInfo = document.createElement("div");
+  productInfo.classList.add("product-info");
+
+  const productInfoDiv = document.createElement("div");
+
+  const productPrice = document.createElement("p");
+  productPrice.innerText = "$" + product.price;
+
+  const productName = document.createElement("p");
+  productName.innerText = product.name;
+
+  productInfoDiv.appendChild(productPrice);
+  productInfoDiv.appendChild(productName);
+
+  const productInfoFigure = document.createElement("figure");
+
+  const productImgCart = document.createElement("img");
+  productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+  productInfoFigure.appendChild(productImgCart);
+
+  productInfo.appendChild(productInfoDiv);
+  productInfo.appendChild(productInfoFigure);
+
+  productCard.appendChild(productImg);
+  productCard.appendChild(productInfo);
+
+  cardsContainer.appendChild(productCard);
 }
